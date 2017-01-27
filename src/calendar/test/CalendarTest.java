dde5f791f0307681e8b198a44823871b0f5ede46
Applied-Programming <aniruddhastapas@gmail.com>
2017-01-27 23:38:05 +0530

    Update

--- src/calendar/test/CalendarTest.java
+++ src/calendar/test/CalendarTest.java
@@ -1,22 +1,20 @@
 package calendar.test;
 
-
 import java.util.*;
 
 public class CalendarTest
 {
 	public static void main(String [] args)
 	{
-		// construct d as current date
 
-		GregorianCalendar d = new GregorianCalendar();
-		int today = d.get(Calendar.DAY_OF_MONTH);
-		int month = d.get(Calendar.MONTH);
+		GregorianCalendar gc = new GregorianCalendar();
+		int today = gc.get(Calendar.DAY_OF_MONTH);
+		int month = gc.get(Calendar.MONTH);
 
-		// set d to start date of the month
-		d.set(Calendar.DAY_OF_MONTH,1);
+		// set gc to start date of the month
+		gc.set(Calendar.DAY_OF_MONTH,1);
 
-		int weekday = d.get(Calendar.DAY_OF_WEEK);
+		int weekday = gc.get(Calendar.DAY_OF_WEEK);
 
 		// print heading
 		System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
@@ -25,7 +23,7 @@ public class CalendarTest
 		do
 		{
 			// print day
-			int day = d.get(Calendar.DAY_OF_MONTH);
+			int day = gc.get(Calendar.DAY_OF_MONTH);
 			if (day < 10) System.out.print(" ");
 			System.out.print(day);
 
@@ -39,12 +37,12 @@ public class CalendarTest
 			if(weekday == Calendar.SATURDAY)
 				System.out.println();
 
-			// advance d to the next day
-			d.add(Calendar.DAY_OF_MONTH,1);
-			weekday = d.get(Calendar.DAY_OF_WEEK);
+			// advance gc to the next day
+			gc.add(Calendar.DAY_OF_MONTH,1);
+			weekday = gc.get(Calendar.DAY_OF_WEEK);
 		}
-		while(d.get(Calendar.MONTH) == month);
-		// the loop exits when d is day 1 of the next month
+		while(gc.get(Calendar.MONTH) == month);
+		// the loop exits when gc is day 1 of the next month
 
 		// print final end of line if necessary
 		if(weekday != Calendar.SUNDAY)
